5e2c73805e42504e32a5074556df38e4aeaf984f
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-21 16:01:10 +0800

    feat: add ai providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,9 +3,18 @@ import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
+  testAI: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, message: "job queued" };
+  }),
+
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
+
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
       name: "test/hello.world",
