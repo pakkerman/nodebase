9a0ed7358ee4cc6892aadd7673bc794fd7ca680e
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-21 20:36:53 +0800

    feat: protected routes

--- src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -1,3 +1,5 @@
+import { requireAuth } from "@/lib/auth-utils";
+
 interface PageProps {
   params: Promise<{
     executionId: string;
@@ -5,6 +7,8 @@ interface PageProps {
 }
 
 const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+
   const { executionId } = await params;
   return <p>Execution Id: {executionId}</p>;
 };
