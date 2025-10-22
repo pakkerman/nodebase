0e773c1fd542f8e5ddc3cead3d316b73f0b4493b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-22 12:10:17 +0800

    feat: add new routes

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,38 +1,8 @@
-import { inngest } from "@/inngest/client";
-import {
-  createTRPCRouter,
-  premiumProcedure,
-  protectedProcedure,
-} from "../init";
-import prisma from "@/lib/db";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAI: premiumProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-    });
-
-    return { success: true, message: "job queued" };
-  }),
-
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: {
-        email: "pakkerman@mail.com",
-      },
-    });
-
-    return prisma.workflow.create({
-      data: {
-        name: "test-workflow",
-      },
-    });
-  }),
+  workflows: workflowsRouter,
 });
 
 export type AppRouter = typeof appRouter;
