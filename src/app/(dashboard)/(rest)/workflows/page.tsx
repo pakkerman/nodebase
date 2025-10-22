cda3b0f8b5c7c57004ae14b80c6acec58c421cde
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-22 18:16:57 +0800

    feat: add workflow API routes

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -5,7 +5,10 @@ import { HydrateClient } from "@/trpc/server";
 import { requireAuth } from "@/lib/auth-utils";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 
-import { WorkflowList } from "@/features/workflows/components/workflows";
+import {
+  WorkflowList,
+  WorkflowsContainer,
+} from "@/features/workflows/components/workflows";
 
 const Page = async () => {
   await requireAuth();
@@ -13,13 +16,15 @@ const Page = async () => {
   prefetchWorkflows();
 
   return (
-    <HydrateClient>
-      <ErrorBoundary fallback={<p>Error!</p>}>
-        <Suspense fallback={<p>Loading...</p>}>
-          <WorkflowList />
-        </Suspense>
-      </ErrorBoundary>
-    </HydrateClient>
+    <WorkflowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error!</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <WorkflowList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsContainer>
   );
 };
 
--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,10 +1,14 @@
 import prisma from "@/lib/db";
-import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  premiumProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
 import { generateSlug } from "random-word-slugs";
 import z from "zod";
 
 export const workflowsRouter = createTRPCRouter({
-  create: protectedProcedure.mutation(({ ctx }) => {
+  create: premiumProcedure.mutation(({ ctx }) => {
     return prisma.workflow.create({
       data: {
         name: generateSlug(3),
