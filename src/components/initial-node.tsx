ffa5d86c4b6ca93a3fbab56442eae8a7ec3f1d9d
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-28 11:16:19 +0800

    fix: open states

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -13,7 +13,7 @@ export const InitialNode = memo((props: NodeProps) => {
 
   return (
     <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
-      <WorkflowNode>
+      <WorkflowNode showToolbar={false}>
         <PlaceholderNode {...props} onClick={() => setSelectorOpen(true)}>
           <div className="cursor-pointer flex items-center justify-center">
             <PlusIcon className="size-4" />
