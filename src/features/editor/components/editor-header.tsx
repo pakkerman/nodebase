a39d039e04387d9f9499b8b0c1300cd430d8bd1c
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-28 12:16:06 +0800

    feat: save workflow functionality

--- src/features/editor/components/editor-header.tsx
+++ src/features/editor/components/editor-header.tsx
@@ -2,11 +2,15 @@
 
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
+import { useAtomValue } from "jotai";
 
 import {
   useSuspenseWorkflow,
+  useUpdateWorkflow,
   useUpdateWorkflowName,
 } from "@/features/workflows/hooks/use-workflows";
+
+import { editorAtom } from "@/features/editor/store/atoms";
 
 import { SaveIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -21,9 +25,25 @@ import {
 } from "@/components/ui/breadcrumb";
 
 export const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+  const editor = useAtomValue(editorAtom);
+  const saveWorkflow = useUpdateWorkflow();
+
+  const handleSave = () => {
+    if (!editor) return;
+
+    const nodes = editor.getNodes();
+    const edges = editor.getEdges();
+
+    saveWorkflow.mutate({
+      id: workflowId,
+      nodes,
+      edges,
+    });
+  };
+
   return (
-    <div className="ml-auto ">
-      <Button size="sm" onClick={() => {}} disabled={false}>
+    <div className="ml-auto">
+      <Button size="sm" onClick={handleSave} disabled={saveWorkflow.isPending}>
         <SaveIcon className="size-4" />
         Save
       </Button>
@@ -97,7 +117,7 @@ export const EditorNameInput = ({ workflowId }: { workflowId: string }) => {
   return (
     <BreadcrumbItem
       onClick={() => setIsEditing(true)}
-      className="cursor-pointer hover:text-foreground transition-colors"
+      className="cursor-pointer transition-colors hover:text-foreground"
     >
       {workflow.name}
     </BreadcrumbItem>
@@ -124,9 +144,9 @@ export const EditorBreadcrumbs = ({ workflowId }: { workflowId: string }) => {
 
 export const EditorHeader = ({ workflowId }: { workflowId: string }) => {
   return (
-    <header className="flex h-14 shrink-0 items-center gap-2 border-b px-4 bg-background">
+    <header className="flex h-14 shrink-0 items-center gap-2 border-b bg-background px-4">
       <SidebarTrigger />
-      <div className="flex flex-row items-center justify-between gap-x-4 w-full">
+      <div className="flex w-full flex-row items-center justify-between gap-x-4">
         <EditorBreadcrumbs workflowId={workflowId} />
         <EditorSaveButton workflowId={workflowId} />
       </div>
