fd4746b3b6c9d24129079054fbcbbb0b90364848
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2025-10-28 13:41:54 +0800

    feat: manual-trigger node setting dialog

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -1,18 +1,24 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { NodeProps } from "@xyflow/react";
 import { BaseTriggerNode } from "../base-trigger-node";
 import { MousePointerIcon } from "lucide-react";
+import { ManualTriggerDialog } from "./dialog";
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleOpenSettings = () => setDialogOpen(true);
+
   return (
     <>
+      <ManualTriggerDialog open={dialogOpen} onOpenChange={setDialogOpen} />
       <BaseTriggerNode
         {...props}
         name="when clicking 'Execute workflow'"
         icon={MousePointerIcon}
         // TODO: status={nodeStatus}
-        // TODO: onSettings={handleOpenSettings}
-        // TODO: onDoubleClick={handleOpenSettings}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
