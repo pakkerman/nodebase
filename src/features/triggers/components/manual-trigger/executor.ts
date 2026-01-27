19033eb6e21aa678688f0913bf0cc88849ae361b
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-01-27 13:26:11 +0800

    feat: manual trigger node status

--- src/features/triggers/components/manual-trigger/executor.ts
+++ src/features/triggers/components/manual-trigger/executor.ts
@@ -1,3 +1,4 @@
+import { manualTriggerChannel } from "@/inngest/channels/manual-trigger";
 import type { NodeExecutor } from "@/features/executions/types";
 
 type ManualTriggerData = Record<string, unknown>;
@@ -5,12 +6,23 @@ export const manualTriggerExecutor: NodeExecutor<ManualTriggerData> = async ({
   nodeId,
   context,
   step,
+  publish,
 }) => {
-  // TODO: publish "Loading" state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "loading",
+    }),
+  );
 
   const result = await step.run("manual-trigger", async () => context);
 
-  // TODO: publish 'success' state for manual trigger
+  await publish(
+    manualTriggerChannel().status({
+      nodeId,
+      status: "success",
+    }),
+  );
 
   return result;
 };
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,6 +6,7 @@ import { inngest } from "./client";
 import { topologicalSort } from "./utils";
 import { getExecutor } from "@/features/executions/lib/executor-registry";
 import { httpRequestChannel } from "./channels/http-request";
+import { manualTriggerChannel } from "./channels/manual-trigger";
 
 export const executeWorkflow = inngest.createFunction(
   {
@@ -14,7 +15,7 @@ export const executeWorkflow = inngest.createFunction(
   },
   {
     event: "workflow/execute.workflow",
-    channels: [httpRequestChannel()],
+    channels: [httpRequestChannel(), manualTriggerChannel()],
   },
   async ({ event, step, publish }) => {
     const workflowId = event.data.workflowId;
