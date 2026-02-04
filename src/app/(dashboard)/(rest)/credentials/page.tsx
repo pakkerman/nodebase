ea16a0977248476b32176fb97321c3dcdfdeab48
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-02-04 16:08:52 +0800

    feat: add page for server loader

--- src/app/(dashboard)/(rest)/credentials/page.tsx
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -1,9 +1,30 @@
-import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { SearchParams } from "nuqs";
 
-const Page = async () => {
+import { requireAuth } from "@/lib/auth-utils";
+import { crednetialsParamsLoader } from "@/features/credentials/server/params-loader";
+import { prefetchCrednetials } from "@/features/credentials/server/prefetch";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
 
-  return <p>Credentials</p>;
+  const params = await crednetialsParamsLoader(searchParams);
+  prefetchCrednetials(params);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <p>TODO: Credential list</p>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
---
+++ src/features/credentials/server/params-loader.ts
@@ -0,0 +1,5 @@
+import { createLoader } from "nuqs/server";
+
+import { credentialsParams } from "../params";
+
+export const crednetialsParamsLoader = createLoader(credentialsParams);
