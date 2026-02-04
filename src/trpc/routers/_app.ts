1e3d3a5e5672d9dacd0108ab6512523427c0a53d
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-02-04 14:21:27 +0800

    feat: add credential router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,11 @@
 import { createTRPCRouter } from "../init";
+
+import { credentialRouter } from "@/features/credentials/server/router";
 import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
   workflows: workflowsRouter,
+  credentials: credentialRouter,
 });
 
 export type AppRouter = typeof appRouter;
