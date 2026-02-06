2ba9a20824a30909ba8bc2a056467cb3e854b620
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2026-02-06 15:27:25 +0800

    fix: require userId when loading credential in backend

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -50,6 +50,16 @@ export const executeWorkflow = inngest.createFunction(
       return topologicalSort(workflow.nodes, workflow.connections);
     });
 
+    const userId = await step.run("find-user-id", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        select: {
+          userId: true,
+        },
+      });
+      return workflow.userId;
+    });
+
     // Init the context with any initial data from the trigger
     let context = event.data.initialData || {};
 
@@ -59,6 +69,7 @@ export const executeWorkflow = inngest.createFunction(
       context = await executor({
         data: node.data as Record<string, unknown>,
         nodeId: node.id,
+        userId,
         context,
         step,
         publish,
